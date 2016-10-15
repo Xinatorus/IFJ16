58dc68818d5f498ea919b518dcd48ce460b664d4
xsztef02 <patrik.sztefek@seznam.cz>
2016-10-15 20:09:21 +0200

    Zakladni popis hash table

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct hTable{
+	char *key;
+	char *type; //1. VELKYM typ var/fce/navesti... 2. datovy typ 3. az n pro parametry F
+	unsigned int id; // pro var 0-n ostatni 0-undefined 1-defined
+	struct hTable *next;
+}HashTable;
+
 int main() {
 
 	printf("hello world\n");
