ec24b100115095d7e75754f34e8a83f122a7a979
xsztef02 <patrik.sztefek@seznam.cz>
2016-10-15 20:21:10 +0200

    Zakladni fce/popis

--- src/main.c
+++ src/main.c
@@ -6,8 +6,15 @@ typedef struct hTable{
 	char *key;
 	char *type; //1. VELKYM typ var/fce/navesti... 2. datovy typ 3. az n pro parametry F
 	unsigned int id; // pro var 0-n ostatni 0-undefined 1-defined
-	struct hTable *next;
+	struct hTable *next; // kvuli konfliktum v omezenosti hashe
 }HashTable;
+
+HashTable *createHashTable();
+int deleteHashTable();
+int addToHashTable();
+int deleteFromHashTable();
+HashTable *searchInHashTable();
+
 
 int main() {
 
