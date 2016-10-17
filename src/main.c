6be9d6bcc111d2f0012a772b0033a1c87ff8ce70
xsztef02 <patrik.sztefek@seznam.cz>
2016-10-17 08:57:24 +0200

    few hash

--- src/main.c
+++ src/main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+/*** MAIN INFO ***
+
+	Jde mit v struct preddefinovane hodnoty? jde mi o null v key pro urciti
+	jestli je dana pozice obsazena nebo ne
+
+*/
 
 
 typedef struct hTable{
-	char *key;
+	char *key; 
 	char *type; //1. VELKYM typ var/fce/navesti... 2. datovy typ 3. az n pro parametry F
-	unsigned int id; // pro var 0-n ostatni 0-undefined 1-defined
+	unsigned int data; // pro var 0-n ostatni 0-undefined 1-defined
 	struct hTable *next; // kvuli konfliktum v omezenosti hashe
 }HashTable;
 
-HashTable *createHashTable();
-int deleteHashTable();
-int addToHashTable();
-int deleteFromHashTable();
-HashTable *searchInHashTable();
+HashTable *createHashTable(unsigned int size);
+int deleteHashTable(HashTable *hashTable);
+int addToHashTable(HashTable *hashTable, char *key,char *type, unsigned int data);
+int deleteFromHashTable(HashTable *HashTable, char *key);
+HashTable *searchInHashTable(HashTable *hashTable, char *key);
+unsigned int hash(char *key);
 
 
 int main() {
@@ -21,3 +31,57 @@ int main() {
 	printf("hello world\n");
 	return 0;
 }
+
+
+//TODO pridat osetreni chyb
+
+HashTable *createHashTable(unsigned int size) {
+	HashTable *htab;
+	htab = malloc(sizeof(HashTable)*size);
+	
+	return htab;
+}
+
+void deleteHashTable(HashTable *hashTable) {
+	free(hashTable);
+}
+
+int addToHashTable(HashTable *hashTable, char *key, char *type, unsigned int data) {
+	//+search jesi neexistuje uz... asi podle null v key
+
+	unsigned int index = hash(key);
+	hashTable[index].key = malloc(sizeof(char)*strlen(key));
+	strcpy(hashTable[index].key, key);
+	
+	hashTable[index].type = malloc(sizeof(char)*strlen(type));
+	strcpy(hashTable[index].type, type);
+
+	hashTable[index].data = data;
+
+	return 0;
+}
+
+// je vubec potreba??
+int deleteFromHashTable(HashTable *HashTable, char *key) {
+	
+}
+
+HashTable *searchInHashTable(HashTable *hashTable, char *key) {
+	unsigned int index = hash(key);
+	if (hashTable[index].key == NULL) { // nenalezen
+		return NULL; // asi?
+	}
+	else { //neco tam je
+		if (strcmp(hashTable[index].key, key)) {
+
+		}
+		else {
+
+		}
+	}
+
+	return &hashTable[index];
+}
+
+
+unsigned int hash(char *key);
