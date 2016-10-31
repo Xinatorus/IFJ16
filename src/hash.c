c2a61b35b7a3b2076e941e279c4530bfc3790214
xsztef02 <patrik.sztefek@seznam.cz>
2016-10-31 23:47:33 +0100

    Opraven hash

--- src/hash.c
+++ src/hash.c
@@ -11,28 +11,52 @@ HashTable *createHashTable(unsigned int size) {
 	htab = malloc(sizeof(HashTable)*size);
 	if (htab == NULL)
 		return NULL;
+
 	//TODO add pre-set val?
+	memset(htab, 0, sizeof(HashTable)*size);
+
 	return htab;
 }
 
 // Odstraneni tabulky
 void deleteHashTable(HashTable *hashTable) {
 	//TODO procistit jednotlive polozky/seznamy 
+
+
 	free(hashTable);
 }
 
 // Pridavani zaznamu do tabulky
 int addToHashTable(HashTable *hashTable, char *key, char *type, unsigned int data) {
-	//TODO search jesi uz neexistuje
+
+	if (searchInHashTable(hashTable, *key) != NULL) //prvek uz existuje!
+		return 1;
 
 	unsigned int index = hash(key);
-	hashTable[index].key = malloc(sizeof(char)*strlen(key));
-	strcpy(hashTable[index].key, key);
+	
+	//TODO pokud na dane pozici neni nejaky prvek, zaradim ho do seznamu
 
-	hashTable[index].type = malloc(sizeof(char)*strlen(type));
-	strcpy(hashTable[index].type, type);
+	HashTable *item = &(hashTable[index]);
 
-	hashTable[index].data = data;
+	if (item->key != NULL) {//pokud neco jiz existuje
+		for (; item->next != NULL; item = item->next); //najdu konec
+		// vytvorim novou polozku
+		HashTable *tmp = malloc(sizeof(HashTable));
+		//pridam do seznamu
+		item->next = tmp;
+		//zmenim odkaz na polozku
+		item = tmp;
+	}
+
+	item->key = malloc(sizeof(char)*strlen(key));
+	strcpy(item->key, key);
+
+	item->type = malloc(sizeof(char)*strlen(type));
+	strcpy(item->type, type);
+
+	item->data = data;
+
+	
 
 	return 0;
 }
@@ -48,12 +72,13 @@ HashTable *searchInHashTable(HashTable *hashTable, char *key) {
 	unsigned int index = hash(key);
 
 	// prochazim zaznam, pokud je key = 0 neexistuje v tabulce, pokud je zaznam nullovy = neexistuje v seznamu
+	// WTF? proc ten item!=0 ... uz je moc pozde....
 	for (HashTable *item = hash(key); item != NULL && item->key != NULL; item = item->next) {
 		if (!strcmp(key, item->key))
 			return item; //vracim odkaz na zaznam
 	}
 
-	// nenaezen
+	// nenalezen
 	return NULL;
 }
 
