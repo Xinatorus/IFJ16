48e7c61167574abf88111a8ee63fac6014ff0ed7
Jakub Štol <stoljakub@seznam.cz>
2016-11-30 20:21:21 +0100

    Add files via upload

--- src/io.c
+++ src/io.c
@@ -22,25 +22,193 @@ bool readString(char *slovo, size_t max_size)
 bool readInt()
 {
     char cislo[STR_MAX];
+    int pocet_znaku;
 
     if (readString(cislo, STR_MAX))
     {
-        int cis = atoi(cislo);
+        pocet_znaku = strlen(cislo);
 
-        printf("%d\n", cis);
-        return true;
+        for (int i = 0;i < pocet_znaku;i++)
+        {
+            if (i == (pocet_znaku - 1) || cislo[pocet_znaku - 1] == ' ')
+            {
+                int vysledek = atoi(cislo);
+                return true;
+            }
 
+            if (!isdigit(cislo[i]))
+            {
+                return false;
+            }
+        }
+
+        return false;
     }
+
     else
     {
-        fprintf(stderr, "stderr - fgets\n");
+        fprintf(stderr, "stderr int - fgets\n");
         return false;
     }
 }
 
 double readDouble()
 {
+    char cislo[STR_MAX];
+    int pocet_znaku;
+    int i = 0;
+    int token = 0;
 
+    if (readString(cislo, STR_MAX))
+    {
+        pocet_znaku = strlen(cislo);
+
+        while (cislo[i] < pocet_znaku)
+        {
+            switch (token)
+            {
+                case 0:
+                    if (isdigit(cislo[i]))
+                    {
+                        token = 1;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+
+                    break;
+
+                case 1:
+                    if (isdigit(cislo[i]))
+                    {
+                        token = 1;
+                    }
+
+                    else if (cislo[i] == '.')
+                    {
+                        token = 10;
+                    }
+
+                    else if (cislo[i] == 'e' || cislo[i] == 'E')
+                    {
+                        token = 2;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+
+                    break;
+
+                case 2:
+                    if (isdigit(cislo[i]) || cislo[i] == '+' || cislo[i] == '-')
+                    {
+                        token = 3;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+
+                    break;
+
+                case 3:
+                    if (isdigit())
+
+
+
+            }
+
+            i++;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*pocet_znaku = strlen(cislo);
+
+        if (isdigit(cislo[0]))
+        {
+            for (int i = 1; i < pocet_znaku; i++)
+            {
+                if ((!isdigit(cislo[i])) || (cislo[i] != '.') || (cislo[i] != 'e') || (cislo[i] != 'E'))
+                {
+                    return false;
+                }
+
+                else if (cislo[i] == '.')
+                {
+                    i++;
+
+                    if (!isdigit(cislo[i]))
+                    {
+                        return false
+                    }
+
+                    else if (i == (pocet_znaku - 1))
+                    {
+                        return false;
+                    }
+
+                    else if ()
+                    {
+
+                    }
+
+                }
+
+                else if ((cislo[i] == 'e') || (cislo[i] == 'E'))
+                {
+
+
+                }
+
+                else if (i == (pocet_znaku - 1))
+                {
+                    return false;
+                }
+            }
+        }
+
+        else
+        {
+            return false;
+        }*/
+    }
+
+    else
+    {
+        fprintf(stderr, "stderr double - fgets\n");
+        return false;
+    }
 }
 
 int main()
@@ -50,7 +218,8 @@ int main()
         printf("%s\n", veta);*/
 
 
-        readInt();
+        //readInt();
+
 
         return 0;
 }
