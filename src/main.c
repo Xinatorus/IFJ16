a1c6f8a170c2935c54c115d029d3cf78007ec181
Marek Sipos <xsipos03@stud.fit.vutbr.cz>
2016-12-06 18:16:13 +0100

    Added output_time

--- src/main.c
+++ src/main.c
@@ -5,41 +5,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define LEX_TEST 0       //Lexical analysis test
-#define VALGRIND_TEST 0  //Valgrind test
+#define LEX_TEST 0       // Lexical analysis test
+#define VALGRIND_TEST 0  // Valgrind test
+#define OUTPUT_TIME 1    // Output execution time 
 
 int main(int argc, char* argv[]) {
-	
+
+    #if OUTPUT_TIME == 1
+        clock_t tStart = clock();
+    #endif
+
 	// Missing parameter
 	if (argc < 2) {
 		error(ERR_INTER);
 	}
-
 	// Load source file
 	loadSource(argv[1]);
 
-#if LEX_TEST == 1
-    /* Lexical analysis test (print all found tokens) */
-	fprintf(stdout, "=== TEST LEXIKALNI ANALYZY ===\n");
-	Ttoken *token = NULL;
-	int num = 0;
-	do {
-		token = getNextToken();
-		fprintf(stdout, "#%-5d Typ: %-11s Data: %-12s\n", num, getTokenName(token->type), token->attr->str);
-		num++;
-	} while (token != NULL && token->type != KONEC_SOUBORU && token->type != LEXIKALNI_CHYBA);
-#elif VALGRIND_TEST == 1
-    /* Valgrind test */
-    tsTest();
-    valgrind();
-    printf("\n\nhit enter to end\n");
-    getchar();
-#else
-    /* Regular syntax analysis */
-    execute();
-#endif
+    #if LEX_TEST == 1
+        /* Lexical analysis test (print all found tokens) */
+	    fprintf(stdout, "=== TEST LEXIKALNI ANALYZY ===\n");
+	    Ttoken *token = NULL;
+	    int num = 0;
+	    do {
+		    token = getNextToken();
+		    fprintf(stdout, "#%-5d Typ: %-11s Data: %-12s\n", num, getTokenName(token->type), token->attr->str);
+		    num++;
+	    } while (token != NULL && token->type != KONEC_SOUBORU && token->type != LEXIKALNI_CHYBA);
+    #elif VALGRIND_TEST == 1
+        /* Valgrind test */
+        tsTest();
+        valgrind();
+        printf("\n\nhit enter to end\n");
+        getchar();
+    #else
 
-return 0;
+
+        /* Regular syntax analysis */
+        execute();
+
+
+    #endif
+
+    #if OUTPUT_TIME == 1
+        printf("Time taken: %.3fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+
+    return 0;
 
 }
