4ed85f1c3b4ac154e0dbbad3a575484529df00f5
xsztef02 <patrik.sztefek@seznam.cz>
2016-12-09 23:26:35 +0100

    repair

--- src/framework.c
+++ src/framework.c
@@ -22,7 +22,7 @@ StackFrame *newFrame(StackFrame *parent, TsTree root, char *name, Data *ret, tIn
 
 	TsTree tsTree = tsFind(root, name);
 
-	int size = tsTree->varCount; // pocet promennych, dohledat v TS
+	int size = tsTree->varCount + 8; // pocet promennych, dohledat v TS
 	StackFrame *sf = malloc(sizeof(StackFrame));
 	if (sf == NULL) return NULL;
 
@@ -36,6 +36,7 @@ StackFrame *newFrame(StackFrame *parent, TsTree root, char *name, Data *ret, tIn
 	if (parent) { //pokud neni prvni
 		sf->child = parent->child; // melo by byt VZDY null! 
 		parent->child = sf;
+		sf->top = parent->top;
 	}
 	else sf->child = NULL;
 	sf->parent = parent;
@@ -73,6 +74,35 @@ StackFrame *newFrame(StackFrame *parent, TsTree root, char *name, Data *ret, tIn
 		}
 	}
 
+	//tmp vars
+	int si = tsTree->varCount;
+	sf->data[si].name = makeString("#tmpI1");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_int;
+	sf->data[si].name = makeString("#tmpI2");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_int;
+	sf->data[si].name = makeString("#tmpI3");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_int;
+
+	sf->data[si].name = makeString("#tmpD1");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_double;
+	sf->data[si].name = makeString("#tmpD2");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_double;
+	sf->data[si].name = makeString("#tmpD3");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_double;
+
+	sf->data[si].name = makeString("#tmpS1");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_string;
+	sf->data[si].name = makeString("#tmpS2");
+	sf->data[si].defined = false;
+	sf->data[si++].type = t_string;
+
 	return sf;
 }
 
@@ -81,8 +111,70 @@ StackFrame *deleteFrame(StackFrame *sf) {
 	StackFrame *tmp = sf->parent;
 	if(tmp!=NULL)
 		sf->parent->child = sf->child;
-	free(sf->data);
+	free(sf->data); //TODO identifikatory a stringy
 	free(sf);
 	return tmp; 
 }
 
+StackFrame *newTopFrame(TsTree root) {
+	int size = 0;
+	debug(" [FRAMEWORK] Creating new frame: %s\n", "#StaticVariables");
+
+	for (TsTree x = root; x != NULL;x=x->next) {
+		size+=x->varCount;
+	}
+
+	StackFrame *sf = malloc(sizeof(StackFrame));
+	if (sf == NULL) return NULL;
+
+	sf->data = malloc(sizeof(Data)*size);
+	if (sf->data == NULL) {
+		free(sf);
+		return NULL;
+	}
+
+	sf->identifier = makeString("#StaticVariables");
+	sf->top = sf;
+	sf->parent = NULL;
+	sf->child = NULL;
+	sf->ret = NULL;
+	sf->size = size;
+
+
+	int index = 0;
+
+	for (TsTree x = root; x != NULL; x = x->next) {
+		debug(" [FRAMEWORK] Searching in %s\n", x->name);
+		hashWriteOut(x->ts);
+		for (unsigned int i = 0; i < HASH_TABLE_SIZE; i++) {
+			//sloupec
+			if (x->ts[i].key != NULL) {
+				for (struct hItem *item = &(x->ts[i]); item != NULL; item = item->next) {
+					if (item->type[0] == 'V') { // pokud je pormenna
+						char *tmp = cat(x->name,".");
+						char *fullname = makeString(cat(tmp,item->key));
+						free(tmp);
+						debug(" [FRAMEWORK] Add to frame..  id: %s, type: %s\n", fullname, item->type);
+						sf->data[index].name = fullname;// jmeno promenne
+						sf->data[index].defined = false;
+						switch (item->type[1]) { //datovy typ
+						case 'I':
+							sf->data[index].type = t_int;
+							break;
+						case 'D':
+							sf->data[index].type = t_double;
+							break;
+						case 'S':
+							sf->data[index].type = t_string;
+							sf->data[index].value.v_string = NULL;
+							break;
+						default: break;
+						}
+						index++;
+					}
+				}
+			}
+		}
+	}
+	return sf;
+}
