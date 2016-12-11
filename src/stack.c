551344b7fe7556241cb5390214970c1b31f21a65
xsztef02 <patrik.sztefek@seznam.cz>
2016-12-12 00:55:26 +0100

    fix function params

--- src/stack.c
+++ src/stack.c
@@ -15,25 +15,25 @@ Stack stackInit(unsigned int size) {
 	return stack;
 }
 
-void stackFree(Stack stack) {
-	free(stack.data);
+void stackFree(Stack *stack) {
+	free(stack->data);
 }
 
-int stackPush(Stack stack, Data data) {
-	if (stack.SP < stack.size) {
-		stack.data[stack.SP++] = data;
+int stackPush(Stack *stack, Data data) {
+	if (stack->SP < stack->size) {
+		stack->data[stack->SP++] = data;
 		return 0;
 	}	
 	else return 1;
 }
 
-int stackPop(Stack stack, Data *data) {
-	if (stack.SP > stack.BP) {
+int stackPop(Stack *stack, Data *data) {
+	if (stack->SP > stack->BP) {
 		/* 
 		musi byt hard copy? ano nebo ne ?
 		data->type = stack.data[stack.SP].type;
 		data->value = stack.data[stack.SP].value;*/
-		*data = stack.data[--stack.SP];
+		*data = stack->data[--stack->SP];
 		return 0;
 	}
 	else return 1; 
