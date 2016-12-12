bbe395e89fb9ccf2d23f7814b13357fd068aab9c
Marek Sipos <xsipos03@stud.fit.vutbr.cz>
2016-12-12 02:41:23 +0100

    gg

--- src/synt_anal.h
+++ src/synt_anal.h
@@ -11,7 +11,7 @@
 #define SYNT_DEBUG 0    // Debug messages for syntax analysis
 #define PREC_DEBUG 0    // Debug messages for precedence analysis
 #define SEM_DEBUG 0     // Debug messsges for semantic analysis
-#define GEN_DEBUG 1     // Debug messages for generator
+#define GEN_DEBUG 0     // Debug messages for generator
 
 #include <stdbool.h>
 #include <string.h>
